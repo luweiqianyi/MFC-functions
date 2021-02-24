a2575b91f8f976a178ab5e8cdb8dc4c379d809c6
luweiqianyi <runningriven@163.com>
2021-02-24 19:14:53 +0800

    截取整个屏幕到某个窗口显示MFC程序

---
+++ MFC11/MFC11Dlg.cpp
@@ -0,0 +1,254 @@
+﻿
+// MFC11Dlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFC11.h"
+#include "MFC11Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC11Dlg 对话框
+
+
+
+CMFC11Dlg::CMFC11Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFC11_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFC11Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFC11Dlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+    ON_BN_CLICKED(IDOK, &CMFC11Dlg::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// CMFC11Dlg 消息处理程序
+
+BOOL CMFC11Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CMFC11Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CMFC11Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+int CMFC11Dlg::CaptureAnImage(HWND hWnd)
+{
+    HDC hdcScreen;
+    HDC hdcWindow;
+    HDC hdcMemDC = NULL;
+    HBITMAP hbmScreen = NULL;
+    BITMAP bmpScreen;
+    DWORD dwBytesWritten = 0;
+    DWORD dwSizeofDIB = 0;
+    HANDLE hFile = NULL;
+    char* lpbitmap = NULL;
+    HANDLE hDIB = NULL;
+    DWORD dwBmpSize = 0;
+
+    // Retrieve the handle to a display device context for the client 
+    // area of the window. 
+    hdcScreen = ::GetDC(NULL);
+    hdcWindow = ::GetDC(hWnd);
+
+    // Create a compatible DC, which is used in a BitBlt from the window DC.
+    // 为当前窗口创建兼容DC
+    hdcMemDC = CreateCompatibleDC(hdcWindow);
+
+    if (!hdcMemDC)
+    {
+        ::MessageBox(hWnd, L"CreateCompatibleDC has failed", L"Failed", MB_OK);
+        goto done;
+    }
+
+    // 获取当前窗口的客户端区域
+    RECT rcClient;
+    ::GetClientRect(hWnd, &rcClient);
+
+    // 设置当前窗口的伸展模式
+    SetStretchBltMode(hdcWindow, HALFTONE);
+
+    // The source DC is the entire screen, and the destination DC is the current window (HWND).
+    // 源DC是整个屏幕，目的DC是当前窗口
+    // The StretchBlt function copies a bitmap from a source rectangle into a destination 
+    // rectangle, stretching or compressing the bitmap to fit the dimensions of the 
+    // destination rectangle, if necessary
+    if (!StretchBlt(hdcWindow,
+        0, 0,
+        rcClient.right, rcClient.bottom,
+        hdcScreen,
+        0, 0,
+        rcClient.right, rcClient.bottom,
+       /* GetSystemMetrics(SM_CXSCREEN),
+        GetSystemMetrics(SM_CYSCREEN),*/
+        SRCCOPY))
+    {
+        ::MessageBox(hWnd, L"StretchBlt has failed", L"Failed", MB_OK);
+        goto done;
+    }
+
+    // Create a compatible bitmap from the Window DC.
+    // 依据当前窗口的dc创建兼容Bitmap
+    hbmScreen = CreateCompatibleBitmap(hdcWindow, rcClient.right - rcClient.left, rcClient.bottom - rcClient.top);
+
+    // 创建失败
+    if (!hbmScreen)
+    {
+        ::MessageBox(hWnd, L"CreateCompatibleBitmap Failed", L"Failed", MB_OK);
+        goto done;
+    }
+
+    // Select the compatible bitmap into the compatible memory DC.
+    // 将当前窗口生成的Bitmap选入当前窗口的DC中
+    SelectObject(hdcMemDC, hbmScreen);
+
+    // Bit block transfer into our compatible memory DC.
+    // 将当前窗口的客户端区域内容拷贝到当前窗口的dc中
+    if (!BitBlt(hdcMemDC,
+        0, 0,
+        rcClient.right - rcClient.left, rcClient.bottom - rcClient.top,
+        hdcWindow,
+        0, 0,
+        SRCCOPY))
+    {
+        ::MessageBox(hWnd, L"BitBlt has failed", L"Failed", MB_OK);
+        goto done;
+    }
+
+    // Get the BITMAP from the HBITMAP.
+    GetObject(hbmScreen, sizeof(BITMAP), &bmpScreen);
+
+    BITMAPFILEHEADER   bmfHeader;
+    BITMAPINFOHEADER   bi;
+
+    bi.biSize = sizeof(BITMAPINFOHEADER);
+    bi.biWidth = bmpScreen.bmWidth;
+    bi.biHeight = bmpScreen.bmHeight;
+    bi.biPlanes = 1;
+    bi.biBitCount = 32;
+    bi.biCompression = BI_RGB;
+    bi.biSizeImage = 0;
+    bi.biXPelsPerMeter = 0;
+    bi.biYPelsPerMeter = 0;
+    bi.biClrUsed = 0;
+    bi.biClrImportant = 0;
+
+    dwBmpSize = ((bmpScreen.bmWidth * bi.biBitCount + 31) / 32) * 4 * bmpScreen.bmHeight;
+
+    // Starting with 32-bit Windows, GlobalAlloc and LocalAlloc are implemented as wrapper functions that 
+    // call HeapAlloc using a handle to the process's default heap. Therefore, GlobalAlloc and LocalAlloc 
+    // have greater overhead than HeapAlloc.
+    hDIB = GlobalAlloc(GHND, dwBmpSize);
+    lpbitmap = (char*)GlobalLock(hDIB);
+
+    // Gets the "bits" from the bitmap, and copies them into a buffer 
+    // that's pointed to by lpbitmap.
+    GetDIBits(hdcWindow, hbmScreen, 0,
+        (UINT)bmpScreen.bmHeight,
+        lpbitmap,
+        (BITMAPINFO*)&bi, DIB_RGB_COLORS);
+
+    // A file is created, this is where we will save the screen capture.
+    hFile = CreateFile(L"captureqwsx.bmp",
+        GENERIC_WRITE,
+        0,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL, NULL);
+
+    // Add the size of the headers to the size of the bitmap to get the total file size.
+    dwSizeofDIB = dwBmpSize + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+    // Offset to where the actual bitmap bits start.
+    bmfHeader.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER);
+
+    // Size of the file.
+    bmfHeader.bfSize = dwSizeofDIB;
+
+    // bfType must always be BM for Bitmaps.
+    bmfHeader.bfType = 0x4D42; // BM.
+
+    WriteFile(hFile, (LPSTR)&bmfHeader, sizeof(BITMAPFILEHEADER), &dwBytesWritten, NULL);
+    WriteFile(hFile, (LPSTR)&bi, sizeof(BITMAPINFOHEADER), &dwBytesWritten, NULL);
+    WriteFile(hFile, (LPSTR)lpbitmap, dwBmpSize, &dwBytesWritten, NULL);
+
+    // Unlock and Free the DIB from the heap.
+    GlobalUnlock(hDIB);
+    GlobalFree(hDIB);
+
+    // Close the handle for the file that was created.
+    CloseHandle(hFile);
+
+    // Clean up.
+done:
+    DeleteObject(hbmScreen);
+    DeleteObject(hdcMemDC);
+    ::ReleaseDC(NULL, hdcScreen);
+    ::ReleaseDC(hWnd, hdcWindow);
+
+    return 0;
+}
+
+void CMFC11Dlg::OnBnClickedOk()
+{
+    // TODO: 在此添加控件通知处理程序代码
+    HWND hwnd = GetSafeHwnd();
+    if (hwnd) {
+        CaptureAnImage(hwnd);
+    }
+}
